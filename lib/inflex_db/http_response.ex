1cc0f8d482288ce8b4e938f580c70001d593dd66
Thiago Santos <thia.mdossantos@gmail.com>
2020-04-10 10:25:43 -0300

    Add http response doc

--- lib/inflex_db/http_response.ex
+++ lib/inflex_db/http_response.ex
@@ -1,4 +1,7 @@
 defmodule InflexDB.HTTPResponse do
+  @moduledoc """
+  Struct representing a HTTP response from InfluxDB.
+  """
   keys = [:status, :headers, :body]
   @enforce_keys keys
   defstruct keys
